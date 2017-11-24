fa6f8a5959121ba2315910cf15a3a4f80ccd429c
Steven Blowers <sblowers@findmypast.com>
2017-11-24 14:02:19 +0000

    player can only move to rooms specified in that room's connections

--- Main.elm
+++ Main.elm
@@ -13,13 +13,17 @@ main =
 -- MODEL
 
 
+type alias Room =
+    { location : Int, connections : List Int }
+
+
 type alias Model =
-    { location : Int, rooms : List Int }
+    { location : Int, rooms : List Room }
 
 
 model : Model
 model =
-    { location = 1, rooms = [ 1, 2, 3 ] }
+    { location = 1, rooms = [ { location = 1, connections = [ 2, 3 ] }, { location = 2, connections = [ 1 ] }, { location = 3, connections = [ 1 ] } ] }
 
 
 
@@ -41,9 +45,32 @@ update msg model =
 -- VIEW
 
 
+find : (a -> Bool) -> List a -> Maybe a
+find predicate list =
+    case list of
+        [] ->
+            Nothing
+
+        first :: rest ->
+            if predicate first then
+                Just first
+            else
+                find predicate rest
+
+
+getConnections : Model -> List Int
+getConnections model =
+    case find (\room -> room.location == model.location) model.rooms of
+        Just room ->
+            room.connections
+
+        Nothing ->
+            []
+
+
 view : Model -> Html Msg
 view model =
     div []
         [ div [] [ text ("You are in Room " ++ (toString model.location)) ]
-        , div [] (List.map (\num -> button [ onClick (Move num) ] [ text ("Go to Room " ++ (toString num)) ]) model.rooms)
+        , div [] (List.map (\num -> button [ onClick (Move num) ] [ text ("Go to Room " ++ (toString num)) ]) (getConnections model))
         ]
