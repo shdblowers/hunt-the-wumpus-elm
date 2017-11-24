d768a4f676f34a1b64c1b07726d3bf7bf3810299
Steven Blowers <sblowers@findmypast.com>
2017-11-24 15:39:43 +0000

    extracting list find to own module

--- Main.elm
+++ Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import ListExtras
 
 
 main : Program Never Model Msg
@@ -25,11 +26,26 @@ model : Model
 model =
     { playerLocation = 1
     , rooms =
-        [ { location = 1, connections = [ 2, 5 ] }
-        , { location = 2, connections = [ 1, 3 ] }
-        , { location = 3, connections = [ 2, 4 ] }
-        , { location = 4, connections = [ 3, 5 ] }
-        , { location = 5, connections = [ 1, 4 ] }
+        [ { location = 1, connections = [ 2, 5, 6 ] }
+        , { location = 2, connections = [ 1, 3, 8 ] }
+        , { location = 3, connections = [ 2, 4, 10 ] }
+        , { location = 4, connections = [ 3, 5, 12 ] }
+        , { location = 5, connections = [ 1, 4, 14 ] }
+        , { location = 6, connections = [ 1, 7, 15 ] }
+        , { location = 7, connections = [ 6, 8, 16 ] }
+        , { location = 8, connections = [ 2, 7, 9 ] }
+        , { location = 9, connections = [ 8, 10, 17 ] }
+        , { location = 10, connections = [ 3, 9, 11 ] }
+        , { location = 11, connections = [ 10, 12, 18 ] }
+        , { location = 12, connections = [ 4, 11, 13 ] }
+        , { location = 13, connections = [ 12, 14, 19 ] }
+        , { location = 14, connections = [ 5, 13, 15 ] }
+        , { location = 15, connections = [ 14, 6, 20 ] }
+        , { location = 16, connections = [ 7, 17, 20 ] }
+        , { location = 17, connections = [ 9, 16, 18 ] }
+        , { location = 18, connections = [ 11, 17, 19 ] }
+        , { location = 19, connections = [ 13, 18, 20 ] }
+        , { location = 20, connections = [ 15, 16, 19 ] }
         ]
     }
 
@@ -53,22 +69,9 @@ update msg model =
 -- VIEW
 
 
-find : (a -> Bool) -> List a -> Maybe a
-find predicate list =
-    case list of
-        [] ->
-            Nothing
-
-        first :: rest ->
-            if predicate first then
-                Just first
-            else
-                find predicate rest
-
-
 getConnections : Model -> List Int
 getConnections model =
-    case find (\room -> room.location == model.playerLocation) model.rooms of
+    case ListExtras.find (\room -> room.location == model.playerLocation) model.rooms of
         Just room ->
             room.connections
 
